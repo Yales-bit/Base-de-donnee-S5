deb04289d27b4e21e1dd603ce944ac62b8d7c43e
hgazaliou01 <hack.gazaliou@insa-strasbourg.fr>
2025-11-18 16:11:38 +0100

    plusieurs_tableaux

--- src/main/java/fr/insa/toto/webui/PageJoueur.java
+++ src/main/java/fr/insa/toto/webui/PageJoueur.java
@@ -7,6 +7,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import fr.insa.beuvron.utils.database.ConnectionPool;
+import fr.insa.beuvron.vaadin.utils.dataGrid.ResultSetGrid;
 import fr.insa.toto.model.Joueur;
 
 import java.sql.Connection;
@@ -37,11 +38,11 @@ public class PageJoueur extends VerticalLayout { // 2. Il faut étendre un Layou
         });
 
         // 4. IMPORTANT : Ajouter les composants à l'écran
-        add(buttonGetJoueurs, gridJoueurs);
+        add(buttonGetJoueurs);
     }
 
     private void refreshGrid() {
-        List<Joueur> listeJoueurs = new ArrayList<>();
+        /*List<Joueur> listeJoueurs = new ArrayList<>();
 
         try (Connection con = ConnectionPool.getConnection()) {
             PreparedStatement pst = con.prepareStatement("SELECT * FROM joueur");
@@ -68,6 +69,14 @@ public class PageJoueur extends VerticalLayout { // 2. Il faut étendre un Layou
             Notification.show("Erreur SQL : " + e.getMessage());
         } catch (Exception e) {
             Notification.show("Erreur : " + e.getMessage());
+        }*/
+        try (Connection con = ConnectionPool.getConnection()){
+            PreparedStatement st = con.prepareStatement( "select surnom,taille,categorie from joueur");
+            ResultSetGrid g = new ResultSetGrid(st);
+            this.add(g);
+
+        } catch(SQLException ex){
+            Notification.show("Erreur : " + ex.getMessage());
         }
     }
 }
