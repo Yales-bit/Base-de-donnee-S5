1dbd36efeb7590afe8970783b72926634bc45ff0
hgazaliou01 <hack.gazaliou@insa-strasbourg.fr>
2025-11-23 00:57:09 +0100

    quasi rien

--- src/main/java/fr/insa/toto/model/TournoiNomExisteDejaException.java
+++ src/main/java/fr/insa/toto/model/TournoiNomExisteDejaException.java
@@ -1,4 +1,4 @@
-package fr.insa.toto.model; // Ou votre package d'exceptions
+package fr.insa.toto.model;
 
 // Cette exception sert juste à signaler précisément ce problème
 public class TournoiNomExisteDejaException extends Exception {
